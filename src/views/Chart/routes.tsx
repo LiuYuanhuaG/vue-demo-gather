2827cebf8f4ed913de3ea4fbc9f055511a3d1daa
xu <mingchulian@qq.com>
2022-08-04 11:02:04 +0800

    增加echarts自适应

---
+++ src/views/Chart/routes.tsx
@@ -0,0 +1,8 @@
+export default {
+  path: "/Chart",
+  name: "Chart",
+  component: () => import("@/views/Chart/Index.vue"),
+  meta: {
+    title: "echarts自适应",
+  },
+}
---
+++ src/views/Word/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">word</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "",
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="less" scoped>
+</style>
