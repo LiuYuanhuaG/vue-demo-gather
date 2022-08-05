1ea119b776d3c1cc9ceebd012b5a3b4fdfa31f11
eternallycyf <a969475322@gmail.com>
2022-08-05 12:11:13 +0800

    test: cancle eslint at the moment

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,10 +1,12 @@
 /* eslint-disable */
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+	import type { DefineComponent } from 'vue';
+	const component: DefineComponent<{}, {}, any>;
+	export default component;
 }
-declare module 'pizzip'
-declare module 'jszip-utils'
-declare module 'file-saver'
-declare module 'docxtemplater'
+declare module 'pizzip';
+declare module 'jszip-utils';
+declare module 'file-saver';
+declare module 'docxtemplater';
+declare module 'howtools';
+declare module 'leaflet';
