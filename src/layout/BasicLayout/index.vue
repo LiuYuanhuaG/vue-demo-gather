678f2a9895794e90b6a2398ed660fa24c47b9cf4
liuyuanhua <1764962071@qq.com>
2022-09-21 14:17:45 +0800

    feat: 添加花里胡哨

--- src/layout/BasicLayout/index.vue
+++ src/layout/BasicLayout/index.vue
@@ -1,104 +1,110 @@
 <template>
-  <a-layout class="basicLayout-wrap">
-    <Header />
-    <a-layout>
-      <SideMenu v-bind="layoutConf" />
-      <a-layout class="basicLayout-content">
-        <!-- breadcrumb -->
-        <Breadcrumb v-if="routeMeta.breadcrumb" @handleClick="handleBreadcrumb" />
+	<a-layout class="basicLayout-wrap">
+		<Header />
+		<a-layout class="cc_on">
+			<SideMenu v-bind="layoutConf" />
+			<a-layout class="basicLayout-content">
+				<!-- breadcrumb -->
+				<Breadcrumb
+					v-if="routeMeta.breadcrumb"
+					@handleClick="handleBreadcrumb"
+				/>
 
-        <!-- content -->
-        <a-layout-content class="layout-content">
-          <!-- divider -->
-          <a-divider v-if="routeMeta.breadcrumb" class="line" />
-          <!-- router-view -->
-          <template v-if="routeMeta.hiddenWrap">
-            <!-- <router-view /> -->
-            <router-view v-slot="{ Component, route }">
-              <transition name="fade-slide" mode="out-in" appear>
-                <!-- https://www.jianshu.com/p/399667ec9ef8 -->
-                <div :key="route.name">
-                  <component :is="Component" />
-                </div>
-              </transition>
-            </router-view>
-          </template>
-          <a-card class="layout-content-a-card" v-else>
-            <!-- <router-view /> -->
-            <router-view v-slot="{ Component, route }">
-              <transition name="fade-slide" mode="out-in" appear>
-                <div :key="route.name">
-                  <component :is="Component" />
-                </div>
-              </transition>
-            </router-view>
-          </a-card>
-        </a-layout-content>
-      </a-layout>
-    </a-layout>
-  </a-layout>
+				<!-- content -->
+				<a-layout-content class="layout-content">
+					<!-- divider -->
+					<a-divider v-if="routeMeta.breadcrumb" class="line" />
+					<!-- router-view -->
+					<template v-if="routeMeta.hiddenWrap">
+						<!-- <router-view /> -->
+						<router-view v-slot="{ Component, route }">
+							<transition name="fade-slide" mode="out-in" appear>
+								<!-- https://www.jianshu.com/p/399667ec9ef8 -->
+								<div :key="route.name">
+									<component :is="Component" />
+								</div>
+							</transition>
+						</router-view>
+					</template>
+					<a-card class="layout-content-a-card" v-else>
+						<!-- <router-view /> -->
+						<router-view v-slot="{ Component, route }">
+							<transition name="fade-slide" mode="out-in" appear>
+								<div :key="route.name">
+									<component :is="Component" />
+								</div>
+							</transition>
+						</router-view>
+					</a-card>
+				</a-layout-content>
+			</a-layout>
+		</a-layout>
+	</a-layout>
 </template>
 <script setup lang="ts">
 import { Route } from 'ant-design-vue/es/breadcrumb/Breadcrumb';
 import Header from './components/Header.vue';
 import SideMenu from './components/SideMenu';
 import { clearMenuItem, filterRoutes } from './utils';
-import router from '@/router'
-import { reactive, computed } from 'vue'
+import router from '@/router';
+import { reactive, computed } from 'vue';
 
 const menuData = filterRoutes(
-  clearMenuItem(router.getRoutes()).filter((n) => n.path.startsWith('/')),
+	clearMenuItem(router.getRoutes()).filter(n => n.path.startsWith('/')),
 );
 
 const layoutConf = reactive({
-  theme: 'light',
-  menuWidth: 208,
-  menuData,
+	theme: 'light',
+	menuWidth: 208,
+	menuData,
 });
 
 const routeMeta = computed(() => router.currentRoute.value.meta);
 
 const breadcrumb = computed(
-  () =>
-    router.currentRoute.value.matched
-      .filter((n) => !['/', '/app'].includes(n.path))
-      .map((item) => {
-        return {
-          path: item.path,
-          breadcrumbName: item.meta.title || '',
-        };
-      }) as Route[],
+	() =>
+		router.currentRoute.value.matched
+			.filter(n => !['/', '/app'].includes(n.path))
+			.map(item => {
+				return {
+					path: item.path,
+					breadcrumbName: item.meta.title || '',
+				};
+			}) as Route[],
 );
 const handleBreadcrumb = () => {
-  const path = breadcrumb.value?.[breadcrumb.value.length - 2]?.path;
-  path && router.push(path);
+	const path = breadcrumb.value?.[breadcrumb.value.length - 2]?.path;
+	path && router.push(path);
 };
 </script>
 <style lang="less" scoped>
+.cc_on {
+	flex-direction: row !important;
+}
 .basicLayout-wrap {
-  overflow: hidden;
-  height: 100vh;
+	overflow: hidden;
+	height: 100vh;
 
-  .basicLayout-content {
-    overflow-y: auto;
-    margin: 0;
-    padding: 24px;
-    height: calc(100vh - 80px);
-    background: '#F0F1F4';
+	.basicLayout-content {
+		overflow-y: auto;
+		margin: 0;
+		padding: 24px;
+		height: calc(100vh - 80px);
+		background: '#F0F1F4';
 
-    .layout-content {
-      flex: 1;
-      // display: flex;
-    }
+		.layout-content {
+			flex: 1;
+			// display: flex;
+		}
 
-    .layout-content-a-card {
-      height: 100%;
+		.layout-content-a-card {
+			height: 100%;
 
-      >div {
-        height: 100%;
-        position: relative;
-      }
-    }
-  }
+			> div {
+				height: 100%;
+				position: relative;
+			}
+		}
+	}
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -2,24 +2,24 @@ const { defineConfig } = require('@vue/cli-service');
 const AutoImport = require('unplugin-auto-import/webpack');
 const Components = require('unplugin-vue-components/webpack');
 const {
-	ElementPlusResolver,
-	AntDesignVueResolver,
+  ElementPlusResolver,
+  AntDesignVueResolver,
 } = require('unplugin-vue-components/resolvers');
 module.exports = defineConfig({
-	transpileDependencies: true,
-	lintOnSave: false, //关闭eslint 报错不影响编译 可选
-	devServer: {
-		open: true,
-	},
-	publicPath: process.env.NODE_ENV === 'production' ? '/vue-demo-gather/' : '/',
-	configureWebpack: {
-		plugins: [
-			AutoImport({
-				resolvers: [ElementPlusResolver(), AntDesignVueResolver()],
-			}),
-			Components({
-				resolvers: [ElementPlusResolver(), AntDesignVueResolver()],
-			}),
-		],
-	},
+  transpileDependencies: true,
+  lintOnSave: false, //关闭eslint 报错不影响编译 可选
+  devServer: {
+    open: true,
+  },
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-demo-gather/' : '/',
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver(), AntDesignVueResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver(), AntDesignVueResolver()],
+      }),
+    ],
+  },
 });
